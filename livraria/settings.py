deb4eb27c5de2fea422e6b0e2e1ccae06b674ae5
LucasIdalino <lucasidalino14@hotmail.com>
2022-05-27 09:48:32 -0300

    atualização host

--- livraria/settings.py
+++ livraria/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-*kxc#2m*i44il1u4++0bfuk#hc0cyefi!r8-^i$y78_@&obl-%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition

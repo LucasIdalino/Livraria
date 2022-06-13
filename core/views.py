ad5c195b9ee16128b575b1c9f2119dbfa246baf1
LucasIdalino <lucasidalino14@hotmail.com>
2022-06-12 21:40:40 -0300

    nothing

--- core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def teste(request):
-    return HttpResponse('Olá Pessoas.')
-
-def second_teste(request):
-    return HttpResponse('Essa é a segunda página.')
